baf9b575e7a369cd55fbf0108d35576fd6ae1f9f
almaavu <almaavu@gmail.com>
2021-09-23 13:09:16 +0200

    initial commit

--- push.sh
+++ push.sh
@@ -5,9 +5,7 @@ scdir="$(dirname $(readlink -f $0))"
 
 read -p "Commit description: " desc
 
-cd $scdir/..
-git add . && \
-git add -u && \
+
 git commit -m "$desc" && \
-git rm -r --cached .
+#git rm -r --cached .
 git push origin main
