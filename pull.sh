08629730cedc3b574add8a9cd73289f83d56dbde
almaavu <almaavu@gmail.com>
2021-09-23 13:20:42 +0200

    2

---
+++ pull.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+git pull origin main --allow-unrelated-histories
--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ scdir="$(dirname $(readlink -f $0))"
 
 read -p "Commit description: " desc
 
-
+git add .
 git commit -m "$desc" && \
 #git rm -r --cached .
 git push origin main
