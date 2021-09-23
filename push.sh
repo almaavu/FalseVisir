4f7cca5ef2def2afc226e7f21e7aec3ea796963e
almaavu <almaavu@gmail.com>
2021-09-23 15:25:36 +0200

    initial commit

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+#git rm -r --cached .
+
+desc=minor edit
+#read -p "Commit description: " desc
+
+git add .
+git commit -m "$desc"
+git push -u origin master
+
+git status
+            
