db214e38e2bb03b313d582a9bf5abf35ff227aa2
almaavu <almaavu@gmail.com>
2021-09-23 13:22:47 +0200

    4

--- push.sh
+++ push.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-scname=$(basename -- "$(readlink -f -- "$0")")
-scdir="$(dirname $(readlink -f $0))"
+
+#git rm -r --cached .
+
 
 read -p "Commit description: " desc
 
 git add .
-git commit -m "$desc" && \
-#git rm -r --cached .
+git commit -m "$desc"
 git push -u origin master
 
