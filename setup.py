7ac365d4984f0e7d48f4580a686ec1849d2eda4b
almaavu <90680039+almaavu@users.noreply.github.com>
2021-10-08 13:14:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,10 @@ setup(
     name='falsevisir',
     version='1.0.0',
     url='https://github.com/almaavu/falsevisir/',
-    author='ffsedd',
-    author_email='ffsedd@gmail.com',
+    author='almaavu',
+    author_email='almaavu@gmail.com',
     description='Combine IR and VIS image to FALSE COLOR',
     packages=find_packages(where=''),  # Required
-    #scripts=['qq'],
     install_requires=['numpy', 'matplotlib', 'imageio', 'scikit-image'],
     include_package_data=True,
 )
